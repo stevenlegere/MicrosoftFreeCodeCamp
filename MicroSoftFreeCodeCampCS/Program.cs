a0d00a0df03286dec8590b3ad44986e1e1efab14
stevenlegere <stevenmarklegere@gmail.com>
2024-05-02 19:50:54 +0100

    Start of FCC

--- MicroSoftFreeCodeCampCS/Program.cs
+++ MicroSoftFreeCodeCampCS/Program.cs
@@ -1,2 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+// https://www.freecodecamp.org/learn/foundational-c-sharp-with-microsoft/write-your-first-code-using-c-sharp/perform-basic-string-formatting-in-c-sharp
+// https://learn.microsoft.com/en-us/training/modules/csharp-literals-variables/
+
+
+Console.Write("Congratulations!");
+Console.Write(" ");
+Console.Write("You wrote your first lines of code.");
+// Output: Congratulations! You wrote your first lines of code.
+
+
+Console.WriteLine("This is the first line.");
+Console.Write("This is the second line.");
+
+// This is the first line.
+// This is the second line.
+
+Console.WriteLine("This is the first line.");
+Console.Write("This is ");
+Console.Write("the second ");
+Console.Write("line.");
+
+Console.WriteLine("\n");
+
+// Which of the following statements are valid?
+//int x = 12.3m
+decimal x = 12.3m;
+//bool x = 'False';
+Console.WriteLine(x);
